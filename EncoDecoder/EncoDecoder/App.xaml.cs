0b9026d6ca180bfcfcc84153ca36028da0790b48
freezpl <freezpl@gmail.com>
2019-03-27 15:46:47 +0200

    some work with UI

--- EncoDecoder/EncoDecoder/App.xaml.cs
+++ EncoDecoder/EncoDecoder/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using QuickConverter;
 
 namespace EncoDecoder
 {
@@ -13,5 +14,23 @@ namespace EncoDecoder
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            EquationTokenizer.AddNamespace(typeof(object));
+            EquationTokenizer.AddNamespace(typeof(System.Windows.Visibility));
+
+            //bool existed;
+            //string id = Marshal.GetTypeLibGuidForAssembly(Assembly.GetExecutingAssembly()).ToString();
+            //MessageBox.Show(id);
+            //Mutex mutex = new Mutex(true, id, out existed);
+            //if (existed == false)
+            //{
+            //    MessageBox.Show("App is already opened");
+            //    Application.Current.Shutdown();
+            //    return;
+            //}
+
+            base.OnStartup(e);
+        }
     }
 }
--- EncoDecoder/EncoDecoder/Models/EncDec.cs
+++ EncoDecoder/EncoDecoder/Models/EncDec.cs
@@ -32,6 +32,7 @@ namespace EncoDecoder.Models
             {
                 isEncrypting = value;
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsEncrypting)));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsWork)));
             }
         }
 
@@ -43,14 +44,19 @@ namespace EncoDecoder.Models
             {
                 isAborting = value;
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsAborting)));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsWork)));
             }
+        }
+
+        public bool IsWork
+        {
+            get { return (isEncrypting || IsAborting) ? true : false; }
         }
 
         FileStream fs;
         Task t;
         long startPoint;
         CancellationTokenSource cts;
-
 
         int partSize;
         public int PartSize
@@ -109,35 +115,35 @@ namespace EncoDecoder.Models
         public EncDec()
         {
             Path = "";
-            PartSize = 10;
+            PartSize = 4096;
             Pass = "abc";
             ProgMax = 4096;
             ProgVal = 0;
 
-            
-            userKey = Registry.CurrentUser;
-            if(userKey != null)
-            {
-                programKey =  userKey.OpenSubKey(PROGRAM_NAME);
-                if (programKey != null)
-                {
-                    MessageBox.Show(programKey.Name);
 
-                }
-            }
+            //userKey = Registry.CurrentUser;
+            //if(userKey != null)
+            //{
+            //    programKey =  userKey.OpenSubKey(PROGRAM_NAME);
+            //    if (programKey != null)
+            //    {
+            //        MessageBox.Show(programKey.Name);
+
+            //    }
+            //}
         }
-        
+
         public void ExitAndSave()
         {
-            if (userKey == null)
-                return;
+            //if (userKey == null)
+            //    return;
 
-            if (programKey == null)
-                programKey = userKey.CreateSubKey(PROGRAM_NAME);
-            
-            programKey.SetValue(nameof(path), path);
-            programKey.SetValue(nameof(startPoint), startPoint);
-            programKey.SetValue(nameof(isEncrypting), isEncrypting);
+            //if (programKey == null)
+            //    programKey = userKey.CreateSubKey(PROGRAM_NAME);
+
+            //programKey.SetValue(nameof(path), path);
+            //programKey.SetValue(nameof(startPoint), startPoint);
+            //programKey.SetValue(nameof(isEncrypting), isEncrypting);
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
@@ -157,62 +163,13 @@ namespace EncoDecoder.Models
                     }
 
                     OpenFileDialog ofd = new OpenFileDialog();
-                    if (ofd.ShowDialog() != true)
-                        return;
-
-                    Path = ofd.FileName;
-                    cts = new CancellationTokenSource();
-                    t = new Task(() =>
+                    if (ofd.ShowDialog() == true)
                     {
-                        try
-                        {
-                            using (fs = new FileStream(path, FileMode.Open, FileAccess.ReadWrite))
-                            {
-                                IsEncrypting = true;
-                                ProgMax = fs.Length;
-
-                                while (startPoint < fs.Length)
-                                {
-                                    if (cts.Token.IsCancellationRequested)
-                                    {
-                                        fs = null;
-                                        MessageBox.Show(startPoint.ToString() );
-                                        return;
-                                    }
-
-                                    long size = (fs.Length - startPoint < PartSize) ? fs.Length - startPoint : PartSize;
-                                    byte[] buffer = new byte[size];
-                                    fs.Read(buffer, 0, buffer.Length);
-
-                                    int cursor = 0;
-                                    for (int i = 0; i < buffer.Length; i++)
-                                    {
-                                        buffer[i] ^= Convert.ToByte(Pass[cursor]);
-                                        cursor++;
-                                        if (cursor >= Pass.Length)
-                                            cursor = 0;
-                                    }
-                                    fs.Seek(startPoint, SeekOrigin.Begin);
-                                    fs.Write(buffer, 0, buffer.Length);
-                                    startPoint = fs.Position;
-                                    ProgVal = startPoint;
-                                    
-                                }
-                                MessageBox.Show("The work is complete!");
-                                IsEncrypting = false;
-                                startPoint = 0;
-                                Path = string.Empty;
-                            }
-                        }
-                        catch (Exception e)
-                        {
-                            MessageBox.Show(e.Message + "\n" + e.StackTrace, "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
-                            return;
-                        }
-                    }, cts.Token);
-
-                    t.Start();
-
+                        Path = ofd.FileName;
+                        startPoint = 0;
+                        IsEncrypting = true;
+                        Encrypting();
+                    }
                 }));
             }
         }
@@ -239,64 +196,78 @@ namespace EncoDecoder.Models
                     cts.Cancel();
                     t.Wait();
 
-                    t = new Task(() =>
-                    {
-                        try
-                        {
-                            using (fs = new FileStream(Path, FileMode.Open, FileAccess.ReadWrite))
-                            {
-                                long length = startPoint;
-                                ProgMax = fs.Length;
-                                startPoint = 0;
-                                while (startPoint < length)
-                                {
-                                    long size = (length - startPoint < PartSize) ? length - startPoint : PartSize;
-                                    byte[] buffer = new byte[size];
-                                    fs.Read(buffer, 0, buffer.Length);
+                    //t = new Task(() =>
+                    //{
+                    //    try
+                    //    {
+                    //        using (fs = new FileStream(Path, FileMode.Open, FileAccess.ReadWrite))
+                    //        {
+                    //            long length = startPoint;
+                    //            ProgMax = fs.Length;
+                    //            startPoint = 0;
+                    //            while (startPoint < length)
+                    //            {
+                    //                long size = (length - startPoint < PartSize) ? length - startPoint : PartSize;
+                    //                byte[] buffer = new byte[size];
+                    //                fs.Read(buffer, 0, buffer.Length);
 
-                                    int cursor = 0;
-                                    for (int i = 0; i < buffer.Length; i++)
-                                    {
-                                        buffer[i] ^= Convert.ToByte(Pass[cursor]);
-                                        cursor++;
-                                        if (cursor >= Pass.Length)
-                                            cursor = 0;
-                                    }
-                                    fs.Seek(startPoint, SeekOrigin.Begin);
-                                    fs.Write(buffer, 0, buffer.Length);
-                                    startPoint = fs.Position;
-                                    ProgVal = length - startPoint;
-                                }
-                                startPoint = 0;
-                                MessageBox.Show("Restore complete!");
-                            }
-                        }
-                        catch (Exception e)
-                        {
-                            MessageBox.Show(e.Message + "\n" + e.StackTrace, "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
-                            return;
-                        }
-                    });
-                    t.Start();
+                    //                int cursor = 0;
+                    //                for (int i = 0; i < buffer.Length; i++)
+                    //                {
+                    //                    buffer[i] ^= Convert.ToByte(Pass[cursor]);
+                    //                    cursor++;
+                    //                    if (cursor >= Pass.Length)
+                    //                        cursor = 0;
+                    //                }
+                    //                fs.Seek(startPoint, SeekOrigin.Begin);
+                    //                fs.Write(buffer, 0, buffer.Length);
+                    //                startPoint = fs.Position;
+                    //                ProgVal = length - startPoint;
+                    //            }
+                    //            startPoint = 0;
+                    //            MessageBox.Show("Restore complete!");
+                    //        }
+                    //    }
+                    //    catch (Exception e)
+                    //    {
+                    //        MessageBox.Show(e.Message + "\n" + e.StackTrace, "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+                    //        return;
+                    //    }
+                    //});
+                    //t.Start();
                 }));
             }
         }
 
         void Encrypting()
         {
+            cts = new CancellationTokenSource();
             t = new Task(() =>
             {
-
                 try
                 {
-                    using (fs = new FileStream(Path, FileMode.Open, FileAccess.ReadWrite))
+                    using (fs = new FileStream(path, FileMode.Open, FileAccess.ReadWrite))
                     {
-                        long length = startPoint;
-                        ProgMax = length;
-                        startPoint = 0;
-                        while (startPoint < length)
+                        ProgMax = fs.Length;
+                        long codeLenght;
+
+                        if (IsEncrypting)
                         {
-                            long size = (length - startPoint < PartSize) ? length - startPoint : PartSize;
+                            codeLenght = fs.Length;
+                        }
+                        else
+                        {
+                            codeLenght = startPoint;
+                        }
+                        while (startPoint < codeLenght)
+                        {
+                            if (cts.Token.IsCancellationRequested)
+                            {
+                                fs = null;
+                                return;
+                            }
+
+                            long size = (codeLenght - startPoint < PartSize) ? codeLenght - startPoint : PartSize;
                             byte[] buffer = new byte[size];
                             fs.Read(buffer, 0, buffer.Length);
 
@@ -312,9 +283,16 @@ namespace EncoDecoder.Models
                             fs.Write(buffer, 0, buffer.Length);
                             startPoint = fs.Position;
                             ProgVal = startPoint;
+
                         }
+
+                        MessageBox.Show("The work is complete!");
+
+                        if (IsEncrypting)
+                            IsEncrypting = false;
+
                         startPoint = 0;
-                        MessageBox.Show("Restore complete!");
+                        Path = string.Empty;
                     }
                 }
                 catch (Exception e)
@@ -322,11 +300,24 @@ namespace EncoDecoder.Models
                     MessageBox.Show(e.Message + "\n" + e.StackTrace, "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
                     return;
                 }
-            });
+            }, cts.Token);
 
             t.Start();
         }
 
+        AppCommand speedCmd;
+        public AppCommand SpeedCmd
+        {
+            get
+            {
+                return speedCmd ?? (speedCmd = new AppCommand((o) =>
+                {
+                    MessageBox.Show("Speed");
+                }));
+            }
+        }
+
+        public void DDD() { }
 
     }
 }
